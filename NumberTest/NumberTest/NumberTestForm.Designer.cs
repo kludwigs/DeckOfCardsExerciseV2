81681021cb7a92d9c0a1dc221c9c6c26c6527cc2
kludwigs <kludwigs@gmail.com>
2014-02-25 01:29:15 -0800

    User Can Input Numbers and display in sorted order

---
+++ NumberTest/NumberTest/NumberTestForm.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace NumberTest
+{
+    partial class NumberTestForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.display_button = new System.Windows.Forms.Button();
+            this.textInputBox1 = new System.Windows.Forms.TextBox();
+            this.submit_button = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ClearButton = new System.Windows.Forms.Button();
+            this.results_label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // display_button
+            // 
+            this.display_button.Location = new System.Drawing.Point(26, 278);
+            this.display_button.Name = "display_button";
+            this.display_button.Size = new System.Drawing.Size(100, 23);
+            this.display_button.TabIndex = 2;
+            this.display_button.Text = "Display/Refresh";
+            this.display_button.UseVisualStyleBackColor = true;
+            this.display_button.Click += new System.EventHandler(this.DisplayButton_Click);
+            // 
+            // textInputBox1
+            // 
+            this.textInputBox1.Location = new System.Drawing.Point(26, 19);
+            this.textInputBox1.Name = "textInputBox1";
+            this.textInputBox1.Size = new System.Drawing.Size(100, 20);
+            this.textInputBox1.TabIndex = 0;
+            // 
+            // submit_button
+            // 
+            this.submit_button.Location = new System.Drawing.Point(132, 16);
+            this.submit_button.Name = "submit_button";
+            this.submit_button.Size = new System.Drawing.Size(75, 23);
+            this.submit_button.TabIndex = 1;
+            this.submit_button.Text = "Submit";
+            this.submit_button.UseVisualStyleBackColor = true;
+            this.submit_button.Click += new System.EventHandler(this.SubmitButton_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(26, 68);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(254, 199);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Enter input here";
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.Location = new System.Drawing.Point(189, 278);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(91, 23);
+            this.ClearButton.TabIndex = 5;
+            this.ClearButton.Text = "Clear";
+            this.ClearButton.UseVisualStyleBackColor = true;
+            this.ClearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // results_label
+            // 
+            this.results_label.AutoSize = true;
+            this.results_label.Location = new System.Drawing.Point(26, 49);
+            this.results_label.Name = "results_label";
+            this.results_label.Size = new System.Drawing.Size(42, 13);
+            this.results_label.TabIndex = 6;
+            this.results_label.Text = "Results";
+            // 
+            // NumberTestForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(368, 322);
+            this.Controls.Add(this.results_label);
+            this.Controls.Add(this.ClearButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.display_button);
+            this.Controls.Add(this.submit_button);
+            this.Controls.Add(this.textInputBox1);
+            this.Name = "NumberTestForm";
+            this.Text = "Enter Numbers To Display in Ascending Order";
+            this.Load += new System.EventHandler(this.NumberTestForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textInputBox1;
+        private System.Windows.Forms.Button submit_button;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button display_button;
+        private System.Windows.Forms.Button ClearButton;
+        private System.Windows.Forms.Label results_label;
+    }
+}
+
---
+++ NumberTest/NumberTest/NumberTestForm.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace NumberTest
+{
+    public partial class NumberTestForm : Form
+    {
+        ArrayList myAL = new ArrayList();
+        static bool sortflag = false;
+        const int MAX_VALUE = 0x7FFF;
+                
+        public NumberTestForm()
+        {
+            InitializeComponent();
+        }
+        /* SubmitButton_Click 
+         * 
+         * Method to process input from the user          
+         * Called from the Submit button to add data to the list        
+         */
+        private void SubmitButton_Click(object sender, EventArgs e)
+        {
+            Process_Input();
+            textInputBox1.Focus();
+        }
+
+        /* method Process_Input
+         * 
+         * Validates and then adds number to list
+         */ 
+        private void Process_Input()
+        {
+                int userValue;
+                short newNum;
+
+                if(int.TryParse(textInputBox1.Text, out userValue))
+                {
+                 // We have the value successfully, range check then add to arraylist
+                    if(RangeCheck(userValue))
+                    {
+                        newNum = (short)userValue;
+                        textInputBox1.Text = "";
+                        AddToList(newNum);
+                    }
+                }
+                else
+                {
+                 // We don't have the users value.
+                 MessageBox.Show("Please enter an integer into the text box.");
+                }
+        }
+        private bool RangeCheck(int value)
+        {
+            if(value < short.MinValue)
+            {
+                MessageBox.Show("Number is less than minimum value -32768 (0x8000)");
+                return false;
+            }
+            else if (value > MAX_VALUE)
+            {
+                MessageBox.Show("Number is greater than maximum value 32767 (0x7FFF)");
+                return false;
+            }
+            else
+                return true;
+        }
+        private void AddToList(short Val)
+        {
+            myAL.Add(Val);
+            /* With an additional entry placd at the end
+             * we can't say that is list sorted so reset flag
+             */ 
+            sortflag = false;
+            /* Confirms to the user what value is submitted */
+            label1.Text = "Last Submitted: " + Val + "";            
+        }
+        /* DisplayButton_Click 
+         * 
+         * method to Called from the Display/Refresh button 
+         * 
+         */
+        private void DisplayButton_Click(object sender, EventArgs e)
+        {
+            DisplayValues();
+            textInputBox1.Focus();
+        }
+        /* method DisplayValues
+         * 
+         * Primary function for displaying values
+         * Calls methods to sort and display number lists
+         */ 
+        private void DisplayValues()
+        {
+            if (myAL.Count != 0)
+            {
+                /* If it's not sorted new entries have been added
+                 * sort the list and refresh the ListView
+                 */ 
+                if (!sortflag)
+                {
+                    myAL.Sort();
+                    sortflag = true;
+                    PrintToListView();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Number List is Empty. Nothing to display");
+            }
+        }
+        /* method: PrintToListView
+         * 
+         * Traverses through the array list 
+         * to display to the user 
+         */
+        private void PrintToListView()
+        {           
+            listView1.Items.Clear();
+            foreach (short elem in myAL)
+            {
+                listView1.Items.Add(new ListViewItem(elem.ToString()));                
+            }           
+        }
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            /* Clear the list and the output screen */
+            myAL.Clear();
+            listView1.Items.Clear();
+            /* reset sortflag and return the info label to default */
+            sortflag = true;
+            label1.Text = "Enter Input Here";
+        }
+
+        private void NumberTestForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
